51fa0e4ee74436fedcc9d74a1f8397b0d546d20a
MuhammadSafril <muhammadsafril1304@gmail.com>
2025-06-14 00:31:17 -0700

    ubah script model climatedata

--- app/Models/ClimateData.php
+++ app/Models/ClimateData.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ClimateData extends Model
 {
-    //
+    public $timestamps = false; // Karena tidak ada `updated_at`
+
+    protected $fillable = [
+        'location',
+        'temperature',
+        'rainfall',
+        'weather_condition',
+        'recorded_at',
+        'created_at'
+    ];
 }
