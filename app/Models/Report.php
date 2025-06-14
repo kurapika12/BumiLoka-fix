d2c447f8776071e9e2efd69d31c1273a54a96140
MuhammadSafril <muhammadsafril1304@gmail.com>
2025-06-14 00:28:02 -0700

    menambahkan script di model report

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    // Daftar kolom yang boleh diisi massal (misal saat create/update)
+    protected $fillable = [
+        'user_id',
+        'category',
+        'location',
+        'description',
+        'photo_url',
+        'status'
+    ];
 }
