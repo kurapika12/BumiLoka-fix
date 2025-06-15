f2e3715481cfaf3770315af57cb1318282881966
ahfan <abangahfan5@gmail.com>
2025-06-15 15:02:33 +0800

    lanjutan

--- resources/views/data-iklim.blade.php blade Copy Edit.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Data Iklim')
-
-@section('content')
-    <h1>Data Iklim</h1>
-    <p>Berikut adalah informasi iklim yang ditampilkan dari BMKG atau data dummy.</p>
-@endsection
