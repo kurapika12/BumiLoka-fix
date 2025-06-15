096dc84b48803a9f6c51ae99912deca06c3356b8
ahfan <abangahfan5@gmail.com>
2025-06-15 14:27:10 +0800

    tambahan

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ Route::get('/edukasi', [EdukasiController::class, 'index'])->name('edukasi');
 Route::get('/data-iklim', [DataIklimController::class, 'index'])->name('data-iklim');
 
 // Laporan
-Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan');
+Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan.index');
 Route::get('/laporan/create', [LaporanController::class, 'create'])->name('laporan.create');
 Route::post('/laporan', [LaporanController::class, 'store'])->name('laporan.store');
